fef45a6c76b32a7028c2ca1c792724ad8a20ddbc
ihsan <ihsanmpm@gmail.com>
2017-12-17 13:14:23 +0530

    Add project files.

---
+++ s o l o/s o l o.cpp
@@ -0,0 +1,17 @@
+// s o l o.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	char str[9] = {'S','o','l','o'};
+	for (int i = 1; i <=8; i++) {
+		std::cout << str[i];
+	}
+
+
+    return 0;
+}
+
